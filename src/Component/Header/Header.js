347707d527ed579adfa5ae9b03a660c00e688738
Hasna Hoque <999hasnahoque@gmail.com>
2020-03-24 14:18:59 +0900

    header structure done

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/logo2.png';
+
+const Header = () => {
+    return (
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <img src={logo} alt=""/>
+            </div>
+            <div class="p-2">
+                <button>Log in</button>
+            </div>
+            <div class="p-2">    
+                <button>Sign Up</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
