3523fde8dfc4f7cd52ce20fe514598126501c37b
Hasna Hoque <999hasnahoque@gmail.com>
2020-03-24 17:11:32 +0900

    until banner structure

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
+import Banner from './Component/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Banner></Banner>
     </div>
   );
 }
---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css'
+import banner from '../../images/bannerbackground.png'
+
+const Banner = () => {
+    return (
+        <div>
+            <img src={banner} alt=""/>
+        </div>
+    );
+};
+
+export default Banner;
