9ca4c705c47cac86284fc6e483dd6c963a07a1cc
Hasna Hoque <999hasnahoque@gmail.com>
2020-03-26 16:35:36 +0900

    Half done of menuDetails.

--- src/Component/MenuDetails/MenuDetails.js
+++ src/Component/MenuDetails/MenuDetails.js
@@ -9,11 +9,24 @@ const MenuDetails = (props) => {
     console.log(currentFood);
     
     return (
-        <div>
-            <h1>{currentFood.name}</h1>
-            <img src={currentFood.images} alt=""/>
-            <p>{currentFood.id}</p>
-            
+        <div className="container my-5 pt-5">
+            <div className="row">
+                <div className="col-md-6">
+                    <h1>{currentFood.name}</h1>
+                    <p className="my-5">{currentFood.fullDescription}</p>
+                    <div className="d-flex  my-4">
+                        <h3>${currentFood.price}</h3>
+                        <div>
+                            <button>-</button>
+                            as
+                            <button>+</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <img src={currentFood.images} alt=""/>
+                </div>          
+            </div>
         </div>
     );
 };
