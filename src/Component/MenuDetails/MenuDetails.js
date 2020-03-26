722ccaf38b408194ca479f6e99aedfb4ce870bb3
Hasna Hoque <999hasnahoque@gmail.com>
2020-03-26 16:02:56 +0900

    solved the problem of Menu Details

--- src/Component/MenuDetails/MenuDetails.js
+++ src/Component/MenuDetails/MenuDetails.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import AllFoods from '../../FakeData/foods.json';
+import allFoods from '../../FakeData/foods.json';
 import { useParams } from 'react-router-dom';
 
 const MenuDetails = (props) => {
     const {id} = useParams();
     console.log(props);
-    const currentFood = AllFoods.find(food =>food.id === props.id);
+    const currentFood = allFoods.find(food=> food.id == id);
     console.log(currentFood);
     
     return (
         <div>
-            {/* <h1>{currentFood.name}</h1>
-            <p>{currentFood.id}</p> */}
-            <p>don't know how to pass the value to menu details</p>
+            <h1>{currentFood.name}</h1>
+            <p>{currentFood.id}</p>
+            <p>{id}</p>
         </div>
     );
 };
