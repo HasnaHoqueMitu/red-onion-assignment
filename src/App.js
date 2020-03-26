9a949921e1a50b8cf5cd76a8f581bdadd41915ec
Hasna Hoque <999hasnahoque@gmail.com>
2020-03-26 09:33:46 +0900

    Setting Router at App.js and staring menu details part

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from './Component/Header/Header';
 import Banner from './Component/Banner/Banner';
 import Nav from './Component/Nav/Nav';
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Banner></Banner>
-      <Nav></Nav>
-    </div>
+    <Router>
+      <switch>
+        <Route exact path="/">
+          <Header></Header>
+          <Banner></Banner>
+          <Nav></Nav>
+        </Route>  
+        <Route path="/food/:id">
+          <Header></Header>
+          
+        </Route>
+        
+      </switch>
+    </Router>
   );
 }
 
